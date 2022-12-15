086536b042c185e7d1d0eedb274d1d435adca04c
EmilianoEscobedo <emilianoescobedo9@gmail.com>
2022-12-14 23:38:15 -0300

    Exercise solved

--- src/com/laingard/HelloWorld.java
+++ src/com/laingard/HelloWorld.java
@@ -2,7 +2,34 @@ package com.laingard;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-    
+        //Data Types
+
+        //numerics
+        byte var1 = 0;
+        short var2 = 1;
+        int var3 = 2;
+        long var4 = 3;
+
+        // numerics - floating point
+        float var5 = 4;
+        double var6 = 5;
+
+        //boolean
+        boolean var7 = true;
+
+        //text
+        char var8 = 'a';
+        String var9 = "Hello World";
+
+        // Sout prints
+        System.out.println(var1);
+        System.out.println(var2);
+        System.out.println(var3);
+        System.out.println(var4);
+        System.out.println(var5);
+        System.out.println(var6);
+        System.out.println(var7);
+        System.out.println(var8);
+        System.out.println(var9);
     }
 }
