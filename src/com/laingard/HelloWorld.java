15ffc519dbb042bc8015f95832e121eb725f5f43
EmilianoEscobedo <emilianoescobedo9@gmail.com>
2022-12-14 23:12:17 -0300

    Hello World in Java!

--- src/com/laingard/HelloWorld.java
+++ src/com/laingard/HelloWorld.java
@@ -1,2 +1,8 @@
-package com.laingard;public class HelloWorld {
+package com.laingard;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    
+    }
 }
